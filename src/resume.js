66103b1dcf04170b99c657074390b6a130f6fd95
Avery2 <justinaverychan@gmail.com>
2026-08-08 23:30:31 -0700

    Keep resume navigation hierarchical

--- src/resume.js
+++ src/resume.js
@@ -1,19 +1,3 @@
 import { initTheme } from './theme.js';
 
 initTheme();
-
-const enteredFromPortfolio = new URL(location.href).searchParams.get('from') === 'portfolio';
-
-if (enteredFromPortfolio) {
-  const backLink = document.querySelector('.content-back-link');
-  if (backLink) {
-    backLink.href = '/';
-    backLink.textContent = '← Back to portfolio';
-  }
-
-  document.querySelectorAll('a[href^="/writing/experience/"], a[href^="/writing/education/"], a[href^="/writing/resume"]').forEach((link) => {
-    const url = new URL(link.href);
-    url.searchParams.set('from', 'portfolio');
-    link.href = `${url.pathname}${url.search}${url.hash}`;
-  });
-}
