8b0a74796f4293688efaf9485b4ae0771415eb13
Avery2 <justinaverychan@gmail.com>
2026-08-08 23:54:03 -0700

    Respect portfolio origin in resume viewer

--- src/resume.js
+++ src/resume.js
@@ -1,3 +1,11 @@
 import { initTheme } from './theme.js';
 
 initTheme();
+
+if (new URL(location.href).searchParams.get('from') === 'portfolio') {
+  const backLink = document.querySelector('.content-back-link');
+  if (backLink) {
+    backLink.href = '/';
+    backLink.textContent = '← Back to portfolio';
+  }
+}
